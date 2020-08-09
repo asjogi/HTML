2ad4ac23d3a9284e45ba80e09419d0ea523eeee0
asjogi <62975468+asjogi@users.noreply.github.com>
2020-08-09 13:16:28 -0400

    Add files via upload
    First commit 08092020

---
+++ file_read.py
@@ -0,0 +1,10 @@
+guest = input("What is your name ? ")
+
+with open('workmodel.txt') as file_read:
+    file_copy = file_read.read()
+
+
+with open('guestname.txt', 'w') as file_object:
+    file_object.write(file_copy)
+    file_object.write('\n' + guest)
+
